b89a0db8e9b6c355500757035fba850a04686f3f
kvrvgv <kvrvgv@yandex.ru>
2024-04-08 19:10:30 +0300

    admin site registration for all models

--- rent/admin.py
+++ rent/admin.py
@@ -1,3 +1,40 @@
 
 
 from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Transport)
+class TransportAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ParkingStation)
+class ParkingStationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Plan)
+class PlansAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.RentPeriod)
+class RentPeriodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.RentPeriodCarUsage)
+class RentPeriodCarUsageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CompanyAccounting)
+class CompanyAccountingAdmin(admin.ModelAdmin):
+    pass
